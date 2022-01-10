28c3383448f744d685abc7bc13bfe9e10a6cf58e
Ben Leighton <benplei@gmail.com>
2022-01-10 06:03:47 +0000

    find package

--- setup.py
+++ setup.py
@@ -1,13 +1,16 @@
+
+
 import setuptools 
 
 setuptools.setup(
     name='xarrayfrac',
     version='0.0.1',
-    url='https://github.com/',
+    url='https://github.com/benjaminleighton/xarrayfrac',
     author='Ben Leighton',
     author_email='benplei@gmail.com',
     description='mandelbrot dynamically generated xarray backend',
-    install_requires=[            # I get to this in a second
+    packages=['xarrayfrac'],
+    install_requires=[
           'xarray',
           'dask',
           'distributed',
