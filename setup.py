174081dedab93bb5bf549c62dafb3b0c45e1c4a7
Ben Leighton <benplei@gmail.com>
2022-01-10 17:37:14 +1100

    more specific numpy dependency

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ long_description = (this_directory / "README.md").read_text()
 
 setuptools.setup(
     name='xarrayfrac',
-    version='0.0.3',
+    version='0.0.4',
     url='https://github.com/benjaminleighton/xarrayfrac',
     author='Ben Leighton',
     author_email='benplei@gmail.com',
@@ -18,7 +18,7 @@ setuptools.setup(
           'xarray',
           'dask',
           'distributed',
-          'numpy',
+          'numpy>=1.21.5',
       ],
     entry_points={
         "xarray.backends": ["xarrayfrac=xarrayfrac.frac_backend:DynamicMandelbrotEntrypoint"],
